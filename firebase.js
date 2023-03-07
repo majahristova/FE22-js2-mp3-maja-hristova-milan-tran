139c13c6122f8f83ef17b2c6ccb8f10e094e5817
Milan <duc.anh.tran@gritacademy.se>
2023-03-07 13:35:04 +0100

    rearranged firebase forloop / added a cart button

--- firebase.js
+++ firebase.js
@@ -1,42 +1,40 @@
-const baseUrl =` https://plantstore-efd58-default-rtdb.europe-west1.firebasedatabase.app/`;
-
+const baseUrl ='https://plantstore-efd58-default-rtdb.europe-west1.firebasedatabase.app/';
 
 async function fetchingProducts(){
-
     const url = baseUrl + 'productinfo.json';
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data)
-displayProduct(data)
-
+    displayProduct(data)
 }
 
 fetchingProducts();
 
-
 function displayProduct(data){
-
     for(let i=0; i< data.length; i++){
-
+        // Create elements for product info
         const nameForProduct = document.createElement('h1');
-        document.body.appendChild(nameForProduct);
-        nameForProduct.innerText = data[i].name;
-        
         const priceForProduct = document.createElement('h3');
-        document.body.appendChild(priceForProduct);
-        priceForProduct.innerText = data[i].price + ' '+ 'kr';
-
         const showImage = document.createElement('img');
-        document.body.appendChild(showImage);
-        showImage.src = data[i].img;
-    
         const leftInStorage = document.createElement('h5');
+        // Create an "Add to cart" button element
+        const addToCartButton = document.createElement('button');
+        addToCartButton.innerText = 'Add to cart';
+        // Attach a click event handler to the button
+        addToCartButton.addEventListener('click', () => {
+            // Add the product to the cart
+            console.log('Adding product to cart:', data[i].name);
+        });
+        // Add the product info and button to the page
+        document.body.appendChild(nameForProduct);
+        document.body.appendChild(priceForProduct);
+        document.body.appendChild(showImage);
         document.body.appendChild(leftInStorage);
-        leftInStorage.innerText = 'Left in storage = ' + data[i].saldo
-
-    
+        document.body.appendChild(addToCartButton);
+        // Set the content of the product info elements
+        nameForProduct.innerText = data[i].name;
+        priceForProduct.innerText = data[i].price + ' kr';
+        showImage.src = data[i].img;
+        leftInStorage.innerText = 'Left in storage = ' + data[i].saldo;
     }
-
-
-  
 }
+
