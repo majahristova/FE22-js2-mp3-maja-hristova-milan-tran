aa18b7145893b049ae8e8d08ddebcce266ffeb90
Milan Tran <milantranducanh@gmail.com>
2023-03-09 17:04:57 +0100

    added total sup of the cart and a clear button

--- modules/checkout.js
+++ modules/checkout.js
@@ -4,9 +4,11 @@ function renderSingleCartItem(cartItem){
     const div = document.querySelector("#checkoutContainer")
     const nameForProduct = document.createElement('h1');
     div.append(nameForProduct);
-
     nameForProduct.innerText = cartItem.name;
 
+    const priceForProduct = document.createElement('h3');
+    div.append(priceForProduct);
+    priceForProduct.innerText = cartItem.price +'kr'
 }
 
 function renderCartItems(){
@@ -15,10 +17,47 @@ function renderCartItems(){
     if(!cartItems){
         return;
     }
+    let totalPrice = 0;
     for(let i = 0; i<cartItems.length; i++){
         let currentCartItem = cartItems[i];
         renderSingleCartItem(currentCartItem);
+        totalPrice += parseInt(currentCartItem.price);
     }
+    const totalElement = document.createElement('h3');
+    totalElement.innerText = `Total Price: ${totalPrice} kr`;
+    document.querySelector("#checkoutContainer").appendChild(totalElement);
+
+    const clearCartButton = document.createElement('button');
+    clearCartButton.innerText = 'Clear Cart';
+    clearCartButton.addEventListener('click', function clearCart(){
+        localStorage.clear();
+        const checkoutContainer = document.querySelector("#checkoutContainer");
+        checkoutContainer.innerHTML = "";
+        const totalElement = document.createElement('h3');
+        totalElement.innerText = `Total Price: 0 kr`;
+        checkoutContainer.appendChild(totalElement);
+      }
+      );
+    document.querySelector("#checkoutContainer").appendChild(clearCartButton);
 }
 
 renderCartItems();
+
+function getTotal(){
+    const cartItems = getCartItems();
+    if(!cartItems){
+      return;
+    }
+    let temp = cartItems.map(function(cartItem){
+      return parseInt(cartItem.price);
+    });
+    let sum = temp.reduce(function(prev, next){
+      return prev + next;
+    }, 0);
+    console.log(sum);
+  }
+getTotal();  
+
+document.querySelector('#purchase-btn').addEventListener('click',() =>{
+    localStorage.clear();
+} )
